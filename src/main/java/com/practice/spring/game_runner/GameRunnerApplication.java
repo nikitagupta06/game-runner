d4781b027e80632599b53f5c8f1a654bee4090df
nikitagupta06 <nikitagupta9696@gmail.com>
2025-06-01 14:47:27 +0530

    first commit

---
+++ src/main/java/com/practice/spring/game_runner/GameRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring.game_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRunnerApplication.class, args);
+	}
+
+}
